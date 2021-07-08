3fcf257dce6d9ced7972bd06a77d9c485a6ef861
Dimitri DarkElf <polaris.vol35@gmail.com>
2021-07-08 17:29:29 +0300

    proverka

---
+++ src/main/java/com/company/QuadraticEquation.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class QuadraticEquation {
+    private double root1;
+    private double root2;
+
+
+    public double[] searchRoot(double a,double b,double c)
+    {
+        double Diskriminant = b*b - 4*a*c;
+        double root[] = new double[2];
+        if (Diskriminant > 0)
+        {
+            root1 = (-b + Math.sqrt(Diskriminant))/(2*a);
+            root2 = (-b - Math.sqrt(Diskriminant))/(2*a);
+            root[0] = root1;
+            root[1] = root2;
+        }
+        if(Diskriminant == 0)
+        {
+            root1 = (-b + Math.sqrt(Diskriminant))/(2*a);
+            root[0] = root1;
+        }
+
+        return root;
+    }
+
+    public double getRoot1()
+    {
+        return root1;
+    }
+    public double getRoot2()
+    {
+        return root2;
+    }
+
+}
---
+++ src/test/java/company/QuadraticEquationTest.java
@@ -0,0 +1,47 @@
+package company;
+
+import com.company.QuadraticEquation;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuadraticEquationTest {
+
+
+    @Test
+    void podschetroot() {
+        QuadraticEquation root = new QuadraticEquation();
+
+        double[] corni = root.searchRoot(1,-4,-5);
+        double[] otvet = new double[]{5,-1} ;                      //заносим получившиеся корни которые должны быть при расчете уравнения
+
+        assertArrayEquals(otvet,corni);
+    }
+
+    @Test
+    void podschetroot2() {
+        QuadraticEquation root = new QuadraticEquation();
+
+        double[] corni = root.searchRoot(1,-6,9);
+        double[] otvet = new double[]{3,0} ;                      //заносим получившиеся корни которые должны быть при расчете уравнения
+
+        assertArrayEquals(otvet,corni);
+    }
+
+    @Test
+    void podschetroot3() {
+        QuadraticEquation root = new QuadraticEquation();
+
+        double[] corni = root.searchRoot(3,-4,2);
+        double[] otvet = new double[]{0,0} ;                      //заносим получившиеся корни которые должны быть при расчете уравнения
+
+        assertArrayEquals(otvet,corni);
+    }
+
+
+
+}
+
+
+
